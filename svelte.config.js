c7268870470ad6249d76a2794cd93b7731ccd7af
Ryan Orlando <jsrco@hotmail.com>
2022-01-24 11:40:55 -0800

    Added TypeScript Support

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,18 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
-import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
-  },
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
 
 export default config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,7 @@
-const config = {
-  mode: "jit",
-  purge: ["./src/**/*.{html,js,svelte,ts}"],
-
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-
   plugins: [],
-};
-
-module.exports = config;
+}
